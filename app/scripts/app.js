00d7a6fda7f94ba86796b007254bf23adb34b55b
Tero Multisilta <tero.multisilta@gmail.com>
2013-06-08 23:24:46 +0300

    Layout modified.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,10 +35,10 @@ angular.module('virtaApp', [])
       'title': '@'
     },
     template:
-      '<div class="navbar navbar-inverse navbar-fixed-top">' +
+      '<div class="navbar navbar-inverse navbar-static-top">' +
         '<div class="navbar-inner">' +
           '<div class="container">' +
-            '<a class="brand" href="#/">{{title}}</a>' +
+            '<a id="logo" class="brand" href="#/">{{title}}</a>' +
             '<ul class="nav" ng-transclude></ul>' +
           '</div>' +
         '</div>' +
