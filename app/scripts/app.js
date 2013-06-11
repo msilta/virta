2e7d7495c7c20af2af4b57b63870d1f1d04a95e7
Tero Multisilta <tero.multisilta@gmail.com>
2013-06-11 20:54:19 +0300

    Extracted navbar directives to their own file.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,44 +21,4 @@ angular.module('virtaApp', [])
 }).run(["$rootScope", "session", function($rootScope, session) {
     return $rootScope.session = session;
   }
-]).directive('navBarTop', function() {
-  return {
-    restrict: 'E',
-    transclude: true,
-    scope: {
-      'title': '@title',
-      'currentUser': '@username',
-      'showNavbar': '@show'
-    },
-    template:
-      '<div class="navbar navbar-inverse navbar-static-top" ng-show="showNavbar">' +
-        '<div class="navbar-inner">' +
-          '<div class="container">' +
-            '<a id="logo" class="brand" href="#/home">{{title}}</a>' +
-            '<ul class="nav" ng-transclude></ul>' +
-            '<div class="plugged-in">{{currentUser}}<span class="divider">|</span>' +
-              '<a href="/#/" title="Plug out">Plug out</a>' +
-            '</div>' +
-          '</div>' +
-        '</div>' +
-      '</div>',
-    replace: true
-  };
-}).directive('navLocation', function($location) {
-  return {
-    restrict: 'E',
-    transclude: true,
-    scope: {
-      'href': '@'
-    },
-    link: function (scope) {
-      scope.location = function (href) {
-        return href.substr(1) === $location.url();
-      };
-    },
-    template: '<li ng-class="{active: location(href)}">' +
-        '<a href="{{href}}" ng-transclude></a>' +
-      '</li>',
-    replace: true
-  };
-});
+]);
